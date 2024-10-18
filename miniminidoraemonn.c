74040397594695f2e26135e7094c3ea897d64437
yanagisawwwa <t236037@st.yamagata-u.ac.jp>
2024-10-18 19:29:25 +0900

    Add files via upload

---
+++ miniminidoraemonn.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int k;
+    printf("How many doraemonns?");
+    
+    scanf("%d", &k);
+    
+    int s;
+    printf("How many minidoraemonn?");
+    scanf("%d", &s);
+    
+    printf("The number of robots is %d",k+s);
+    return 0;
+}
